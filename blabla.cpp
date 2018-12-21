f1eccd7b5023140c13cf4827cbf3adffd8df854d
Dvir Fred <friedvir1@gmail.com>
2018-12-21 09:41:43 +0200

    start working on shuntning-yard function for parser

--- blabla.cpp
+++ blabla.cpp
@@ -3,9 +3,35 @@
 //
 
 #include <string>
+#include <vector>
+#include <stack>
+#include "IExpression.h"
 
 using namespace std;
 
-void ShuntingYard(string[] buffer) {
+void ShuntingYard(vector<string> tokens) {
+    stack <double> values;
+    stack <string> operators;
+
+}
+
+int precedence (string op) {
+    if (!(op.compare("AND")) || !(op.compare("OR"))) {
+        return 1;
+    }
+    if (!(op.compare(">")) || !(op.compare("<")) ||
+    !(op.compare("==")) || !(op.compare("!=")) || !(op.compare(">=")) || !(op.compare("<="))) {
+        return 2;
+    }
+    if (!(op.compare("+")) || !(op.compare("-"))) {
+        return 3;
+    }
+    if (!(op.compare("*")) || !(op.compare("/"))) {
+        return 4;
+    }
+    return 0;
+}
+
+IExpression *apllOp () {
 
 }

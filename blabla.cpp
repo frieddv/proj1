ac9b07ada950f616f97fab739f73fe6303488d80
Dvir Fred <friedvir1@gmail.com>
2018-12-22 21:00:58 +0200

    start working on shuntning-yard-2

--- blabla.cpp
+++ blabla.cpp
@@ -6,13 +6,44 @@
 #include <vector>
 #include <stack>
 #include "IExpression.h"
+#include "UnaryExp.h"
+#include "Var.h"
 
 using namespace std;
 
-void ShuntingYard(vector<string> tokens) {
-    stack <double> values;
-    stack <string> operators;
+bool isNumber(string token) {
 
+}
+
+void ShuntingYard(vector<string> tokens) {
+    stack <IExpression> values;
+    stack <string> operators;
+    int flag = 0;
+    std::vector<string>:: iterator it = tokens.begin();
+    for (; it != tokens.end(); it++) {
+        if (*it == "(") {
+            operators.push(*it);
+            flag = 1;
+        }
+        else if (isNumber(*it)) {
+            IExpression *exp = new Number(stoi(*it);
+            values.push(*exp);
+        }
+        else if (isVarName(*it)) {
+            //todo : fix how to see if its already in maps, means  - is it fine to add pointers to maps in this class
+            IExpression *exp = new Var(*it);
+            values.push(*exp);
+        }
+        else if (*it == ")") {
+            while (!operators.empty() && operators.top() == "(") {
+                IExpression &exp1 = values.top();
+                values.pop();
+                IExpression &exp2 = values.top();
+                values.pop();
+
+            }
+        }
+    }
 }
 
 int precedence (string op) {

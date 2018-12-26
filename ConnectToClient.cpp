d28679a4a67727217cf6a243e5ed3406474557cd
Dvir Fred <friedvir1@gmail.com>
2018-12-26 23:06:40 +0200

    working on ConnectToClient

--- ConnectToClient.cpp
+++ ConnectToClient.cpp
@@ -34,6 +34,9 @@ void *ConnectToClient(void *args) {
         exit(0);
     }
 
+
+
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
